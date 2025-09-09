9acea3cf32b90c1bda5ffef4758d89823d8b6169
SyanRaval <83851802+SyanRaval@users.noreply.github.com>
2025-09-09 17:40:51 +0000

    Resolved issues with filename and field names

--- python/nursing geomatcher/app.py
+++ python/nursing geomatcher/app.py
@@ -6,7 +6,7 @@ from streamlit_folium import st_folium
 st.title("Wanderly GeoMatcher: Geospatial Job Matching Demo")
 
 # Load mock jobs
-jobs = pd.read_csv("jobs.csv")
+jobs = pd.read_csv("sample jobs.csv")
 
 # Initialize map (US-centered)
 m = folium.Map(location=[39.8283, -98.5795], zoom_start=4)
@@ -14,8 +14,8 @@ m = folium.Map(location=[39.8283, -98.5795], zoom_start=4)
 # Add job markers
 for _, job in jobs.iterrows():
     folium.Marker(
-        [job['latitude'], job['longitude']],
-        popup=f"<b>{job['title']}</b><br>Specialty: {job['specialty']}<br>Pay: {job['pay']}",
+        [job['Latitude'], job['Longitude']],
+        popup=f"<b>{job['Profession']}</b><br>Specialty: {job['Specialty']}<br>Pay: {job['Pay']}",
         icon=folium.Icon(color='red')
     ).add_to(m)
 
